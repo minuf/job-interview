405e33479c1465153c8da9b9786ab870f4389541
jorge <j.colorado@peertopark.com>
2016-04-21 22:46:47 +0200

    added Singleton class -MySingletonVolley- to manage the requests queue

--- app/src/main/java/com/example/jorge/job_interview/MainActivity.java
+++ app/src/main/java/com/example/jorge/job_interview/MainActivity.java
@@ -3,11 +3,44 @@ package com.example.jorge.job_interview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.jorge.job_interview.classes.MySingletonVolley;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String url = "http://httpbin.org/html";
+
+// Request a string response
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        // Result handling
+                        System.out.println(response);
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                // Error handling
+                System.out.println("Something went wrong!");
+                error.printStackTrace();
+
+            }
+        });
+
+// Add the request to the queue
+        MySingletonVolley.getInstance(this).addToRequestQueue(stringRequest);
     }
 }
